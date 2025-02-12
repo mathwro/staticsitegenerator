b1afb7aac5c644dd282e3501ac8c3839764a8ae1
Mathias Wrobel <mathias@wrobel.dk>
2025-02-12 20:32:20 +0100

    Cleanup

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,31 +1,23 @@
 class HTMLNode:
-  def __init__(self, tag=None, value=None, children=None, props=None):
-    self.tag = tag
-    self.value = value
-    self.children = children
-    self.props = props
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
 
-  def to_html(self):
-    raise NotImplementedError
-  
-  def props_to_html(self):
-    if self.props is None:
-      return ""
-    result = ""
-    for prop in self.props:
-      props_html += f' {prop}="{self.props[prop]}"'
-    return result
-  
-  def __eq__(self, other):
-    return (
-        self.tag == other.tag
-        and self.value == other.value
-        and self.children == other.children
-        and self.props == other.props
-    )
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
 
-  def __repr__(self):
-    print = f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
 
 
 class LeafNode(HTMLNode):
